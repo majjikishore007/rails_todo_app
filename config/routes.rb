bbd4e50777095df99e4d29e20234cdbaf10398ba
majjikishore007 <majjikishore031@gmail.com>
2024-12-06 19:07:42 +0530

    create crud operations for todo and add browser title

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "todos#index"
-  get "/todos", to: "todos#index"
-  
+  # get "/todos", to: "todos#index"
+  # get "/todos/:id", to: "todos#show"
+  resources :todos
   get "up" => "rails/health#show", as: :rails_health_check
-
 end
